cac5c4f5a81ea137ad2a6620ffdd2198cada876d
Camjkz <camjkz@hotmail.com>
2025-03-31 17:43:46 +0800

    POSITION OF HITSPARKS CHANGE NOW YES

--- characters/EmerlEnemy/DetectHit.gd
+++ characters/EmerlEnemy/DetectHit.gd
@@ -17,11 +17,13 @@ func _process(delta):
 			var colx = get_collision_point(0).x
 			var coly = get_collision_point(0).y
 			print(get_collision_point(0))
+			print(owner.global_position)
 			var lighthitvfx = lighthit.instantiate()
 			add_child(lighthitvfx)
 			hit = 1
-			lighthitvfx.position.x = 0
-			lighthitvfx.position.y = 0
+			lighthitvfx.position.x = (colx - owner.global_position.x)/4
+			lighthitvfx.position.y = (coly - owner.global_position.y)/4
+			print(lighthitvfx.global_position)
 	elif hit == 1:
 		if !is_colliding():
 			hit = 0
